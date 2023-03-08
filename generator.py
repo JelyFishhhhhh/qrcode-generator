52fbfb0ec19a19ae8c7b3f709bcc14927e53dc6c
JelyFishhhhhh <tony0940419@gmail.com>
2023-03-08 08:44:35 +0800

    Commit

--- generator.py
+++ generator.py
@@ -5,6 +5,10 @@ from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 from qrcode.image.styles.colormasks import SolidFillColorMask
 from datetime import datetime
 
+def hex2rgb(code):
+    code = code.lstrip("#")
+    return tuple(int(code[i:i+2], 16) for i in (0, 2, 4))
+
 dt = datetime.now().strftime("%Y%m%d %H-%M-%S")
 
 qr = QRCode(
@@ -15,10 +19,12 @@ qr = QRCode(
 )
 
 input_data = input("Input the url...\n> ")
-# backgrd_code = input("Input the RGB...\n> ")
-# code_code = input("Input the RGB...\n> ")
+backgrd_code = hex2rgb(input("Input the backgrd RGB...\n> "))
+code_code = hex2rgb(input("Input the code RGB...\n> "))
+
+print(f"backgrd_code:{backgrd_code}, code_code:{code_code}")
 
 qr.add_data(input_data)
 qr.make(fit=True)
-img = qr.make_image(image_factory=StyledPilImage, color_mask = SolidFillColorMask((22, 25, 27), (149, 165, 166)), module_drawer = RoundedModuleDrawer())
+img = qr.make_image(image_factory=StyledPilImage, color_mask = SolidFillColorMask(backgrd_code, code_code), module_drawer = RoundedModuleDrawer())
 img.save(f"{dt}_output.png")
