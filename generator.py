cf93c80c3e793ab36ce4df8dea4287d73817b06c
JelyFishhhhhh <tony0940419@gmail.com>
2023-03-08 08:58:21 +0800

    Commit

--- generator.py
+++ generator.py
@@ -4,6 +4,9 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 from qrcode.image.styles.colormasks import SolidFillColorMask
 from datetime import datetime
+import cv2
+
+
 
 def hex2rgb(code):
     code = code.lstrip("#")
@@ -22,9 +25,12 @@ input_data = input("Input the url...\n> ")
 backgrd_code = hex2rgb(input("Input the backgrd RGB...\n> "))
 code_code = hex2rgb(input("Input the code RGB...\n> "))
 
-print(f"backgrd_code:{backgrd_code}, code_code:{code_code}")
+# print(f"backgrd_code:{backgrd_code}, code_code:{code_code}")
 
 qr.add_data(input_data)
 qr.make(fit=True)
 img = qr.make_image(image_factory=StyledPilImage, color_mask = SolidFillColorMask(backgrd_code, code_code), module_drawer = RoundedModuleDrawer())
 img.save(f"{dt}_output.png")
+print("img saved.")
+img = cv2.imread(f"{dt}_output.png")
+cv2.imshow(f"{dt}_output", img)
